4c41bcac2c09bd41af11130dc635e55ed7339a41
spartan077 <saatvik.tiwari2022@vitstudent.ac.in>
2025-11-19 12:28:30 +0530

    Refactor: Use stack-based approach for optimal enemy skip selection

--- sde-soln.py
+++ sde-soln.py
@@ -1,13 +1,25 @@
 def chakrvyu_game(p, enemies, skips, recharges):
-    dictt={}
+    #dictt={}
     #for i,v in enumerate(enemies):
-    #    dictt[v]=i
-    enemy_skipped=sorted(enemies,reverse=True)[:skips]
-    skip_pos=set()
-    for skip_power in enemy_skipped:
-        for i in range(len(enemies)):
-            if enemies[i] == skip_power and i not in skip_pos:
-                skip_pos.add(i)
+    ##    dictt[v]=i
+    ##enemy_skipped=sorted(enemies,reverse=True)[:skips]
+    #skip_pos=set()
+    #for skip_power in enemy_skipped:
+    #    for i in range(len(enemies)):
+    #        if enemies[i] == skip_power and i not in skip_pos:
+    #            skip_pos.add(i)
+    stack = []
+    to_skip = skips
+    
+    
+    for i, power in enumerate(enemies):
+        stack.append((power, i))
+    
+    
+    stack.sort(reverse=True)
+    skip_pos = set()
+    for j in range(min(to_skip, len(stack))):
+        skip_pos.add(stack[j][1])
     curr_power=p
     skips_used=0
     recharges_curr=recharges
